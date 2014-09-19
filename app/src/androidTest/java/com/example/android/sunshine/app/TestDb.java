5adae94418850c4cdfde0456168c2f54eb48d7e7
Paul Shi <mail.paulshi@gmail.com>
2014-09-19 01:45:28 -0400

    Added insert and read test for SQLiteDB

--- app/src/androidTest/java/com/example/android/sunshine/app/TestDb.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/TestDb.java
@@ -1,19 +1,92 @@
 package com.example.android.sunshine.app;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import com.example.android.sunshine.app.data.WeatherContract.LocationEntry;
 import com.example.android.sunshine.app.data.WeatherDbHelper;
 
 /**
  * Created by paulshi on 9/18/14.
  */
 public class TestDb extends AndroidTestCase{
+
+    public static final String LOG_TAG = TestDb.class.getSimpleName();
+
     public void testCreateDb() throws Throwable {
         mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
         SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
         assertEquals(true, db.isOpen());
         db.close();
+    }
+
+    public void testInsertReadDb() {
+        //Test data
+        String testName = "North Pole";
+        String testLocationSetting = "99705";
+        double testLatitude = 64.772;
+        double testLongitude = -147.355;
+
+        WeatherDbHelper dbHelper =
+                new WeatherDbHelper(mContext);
+        SQLiteDatabase sqLiteDb = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+
+        long locationRowId;
+        locationRowId = sqLiteDb.insert(LocationEntry.TABLE_NAME, null, values);
+
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: " + locationRowId);
+
+        String[] columns = {
+            LocationEntry._ID,
+                LocationEntry.COLUMN_LOCATION_SETTING,
+                LocationEntry.COLUMN_CITY_NAME,
+                LocationEntry.COLUMN_COORD_LAT,
+                LocationEntry.COLUMN_COORD_LONG
+        };
+
+        Cursor cursor = sqLiteDb.query(
+                LocationEntry.TABLE_NAME, //Table to Query
+                columns,
+                null, //Columns for the "where" clause
+                null, //Values for the "where" clause
+                null, //columns to group by
+                null, //columns to filter by row groups
+                null //sort order
+        );
+
+        if (cursor.moveToFirst()) {
+            //Get value in each column by finding column index
+            int locationIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LOCATION_SETTING);
+            String location = cursor.getString(locationIndex);
+
+            int nameIndex = cursor.getColumnIndex(LocationEntry.COLUMN_CITY_NAME);
+            String name = cursor.getString(nameIndex);
+
+            int latIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LAT);
+            String latitude = cursor.getString(latIndex);
+
+            int longIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LONG);
+            String longitude = cursor.getString(longIndex);
+
+            assertEquals(testName, name);
+            assertEquals(testLocationSetting, location);
+            assertEquals(testLatitude, latitude);
+            assertEquals(testLongitude, longitude);
+
+        }
+        else {
+            fail("No values returned. Womp Womp :(");
+        }
     }
 
 }
